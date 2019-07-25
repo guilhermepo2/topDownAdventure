58982e47e7fbb4595de506b766a3fd2b0efcd015
Guilherme Oliveira <guilherme.061@gmail.com>
2019-07-25 10:05:56 -0400

    adding pokemon type

--- TopDownAdventure/Assets/Scripts/Combat System/Pokemon/Pokemon.cs
+++ TopDownAdventure/Assets/Scripts/Combat System/Pokemon/Pokemon.cs
@@ -5,6 +5,27 @@ using UnityEngine;
 namespace CombatSystem {
     [CreateAssetMenu(fileName = "Pokemon", menuName = "Combat System/Pokemon")]
     public class Pokemon : ScriptableObject {
+        public enum EPokemonType {
+            Normal,
+            Fire,
+            Fighting,
+            Water,
+            Flying,
+            Grass,
+            Poison,
+            Electric,
+            Ground,
+            Psychic,
+            Rock,
+            Ice,
+            Bug,
+            Dragon,
+            Ghost,
+            Dark,
+            Steel,
+            Fairy
+        }
+
         public enum EExperienceTypes {
             Fast,
             MediumFast,
@@ -18,5 +39,6 @@ namespace CombatSystem {
         public Data.Stats baseStats;
         public int baseExperience;
         public EExperienceTypes experienceType;
+        public EPokemonType pokemonType;
     }
 }
